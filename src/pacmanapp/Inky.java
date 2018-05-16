9238d5eee0804546a64394430b4c8c3c6e64836a
Nicholas Scalice <nbs412@gmail.com>
2018-05-16 10:37:25 -0400

    Inky, some bits of Scoreboard

---
+++ src/pacmanapp/Inky.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacmanapp;
+
+import java.util.Random;
+
+/**
+ *
+ * @author csc190
+ */
+public class Inky implements Sprite {
+
+    protected int x, y, sx, sy;
+    //protected int picIdx = 0;
+    protected String pic = "inky.png";
+
+    public Inky(int x, int y, int sx, int sy) {
+        this.x = x;
+        this.y = y;
+        this.sx = sx;
+        this.sy = sy;
+    }
+
+    public void setDirection(int sx, int sy) {
+
+        this.sx = sx;
+        this.sy = sy;
+    }
+
+    @Override
+    public void draw(API api) {
+        api.drawImg(pic, x, y, 50, 50);
+    }
+
+    private int counter = 0;
+
+    @Override
+    public void update() {
+        //1. get the dx,dy of pacman
+        Pacman p1 = GameEngine.getInst().getPacman();
+        int dx = p1.getX();
+        int dy = p1.getY();
+
+        //2. decide what is the right direction to go to pacman
+        /**
+         * Map map = Map.getInstance(); int dir = map.decideDirection(this.x,
+         * this.y, dx, dy);
+         */
+        //3. update the x,y coordinates
+        int[] arrSx = new int[]{1, 0, -1, 0};
+        int[] arrSy = new int[]{0, -1, 0, 1};
+        int dir = 0; //actually above
+        sx = arrSx[dir];
+        sy = arrSy[dir];
+
+        counter++;
+        this.x += sx;
+        this.y += sy;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int getW() {
+        return 50;
+    }
+
+    @Override
+    public int getH() {
+        return 50;
+    }
+
+    protected int decideDirection(int dx, int dy) {
+        int distanceX = this.x - dx;
+        int distanceY = this.y - dy;
+        boolean flag = true;
+        if (distanceX == 0 && distanceY == 0) {
+            return 0;
+        } else {
+            
+            Random random = new Random();
+            int num = random.nextInt(2)+1;
+            if(num == 1) flag = false;
+            if (Math.abs(distanceX) > Math.abs(distanceY)) {
+                if (distanceX > 0) {
+                    //setDirection(1, 0);
+                    if(flag) return 0;   //East
+                    else return 2;
+                } else {
+                    //setDirection(-1, 0);
+                    if(flag) return 2;   //West
+                    else return 0;
+                }
+            } else {
+                if (distanceY > 0) {
+                    //setDirection(0, -1);
+                    if(flag) return 1;   //North
+                    else return 3;
+                } else {
+                    //setDirection(0, 1);
+                    if(flag) return 3;   //South
+                    else return 1;
+                }
+            }
+        }
+
+    }
+
+}
